10b06b39bf96d22bc8a04d9b0544a137736fe2cb
Daniel Benavides <daiel97dot2@gmail.com>
2020-02-03 12:04:53 -0500

    version 1

---
+++ dist/index.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = __importDefault(require("./classes/server"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_fileupload_1 = __importDefault(require("express-fileupload"));
+const usuario_1 = __importDefault(require("./routes/usuario"));
+const post_1 = __importDefault(require("./routes/post"));
+const cors_1 = __importDefault(require("cors"));
+const usuarioP_1 = __importDefault(require("./routes/usuarioP"));
+const posttienda_1 = __importDefault(require("./routes/posttienda"));
+const server = new server_1.default();
+// Body parser
+server.app.use(body_parser_1.default.urlencoded({ extended: true }));
+server.app.use(body_parser_1.default.json());
+// FileUpload
+server.app.use(express_fileupload_1.default({ useTempFiles: true }));
+//configurar cors
+server.app.use(cors_1.default({ origin: true, credentials: true }));
+// Rutas de mi app
+server.app.use('/user', usuario_1.default);
+server.app.use('/userP', usuarioP_1.default);
+server.app.use('/posts', post_1.default);
+server.app.use('/postsT', posttienda_1.default);
+// Conectar DB
+mongoose_1.default.connect('mongodb+srv://admin:peluditos@cluster0-3o4h8.mongodb.net/PeluditosDB?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+    if (err)
+        throw err;
+    console.log('Base de datos ONLINE');
+});
+// Levantar express
+server.start(() => {
+    console.log(`Servidor corriendo en puerto ${server.port}`);
+});
