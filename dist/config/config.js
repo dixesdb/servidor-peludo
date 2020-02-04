ada6ee7f74bf2938f87b84d4e2b8ceb36d332b96
Daniel Benavides <daiel97dot2@gmail.com>
2020-02-04 10:15:59 -0500

    version 3

---
+++ dist/config/config.js
@@ -0,0 +1,17 @@
+//+=============================================
+//Configuracion de puertos
+//=============================================
+process.env.PORT = process.env.PORT || 3000;
+///============================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+///===========================================
+let urlDB;
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/fotosgram';
+}else{
+    urlDB = 'mongodb+srv://admin:peluditos@cluster0-3o4h8.mongodb.net/PeluditosDB?retryWrites=true&w=majority';
+}
+
+process.env.urlDB = urlDB;
+
+
--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,10 @@ const cors_1 = __importDefault(require("cors"));
 const usuarioP_1 = __importDefault(require("./routes/usuarioP"));
 const posttienda_1 = __importDefault(require("./routes/posttienda"));
 const server = new server_1.default();
+require('./config/config');
+server.port=process.env.PORT;
+
+
 // Body parser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
@@ -26,7 +30,7 @@ server.app.use('/userP', usuarioP_1.default);
 server.app.use('/posts', post_1.default);
 server.app.use('/postsT', posttienda_1.default);
 // Conectar DB
-mongoose_1.default.connect('mongodb+srv://admin:peluditos@cluster0-3o4h8.mongodb.net/PeluditosDB?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+mongoose_1.default.connect(process.env.urlDB, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
     if (err)
         throw err;
     console.log('Base de datos ONLINE');
